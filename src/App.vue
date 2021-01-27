60c402fcb0f288dda441ad86e123ba82ef9ea994
limhoooo <dlagh123@gmail.com>
2021-01-27 14:30:46 +0900

    es6 리팩토링

--- src/App.vue
+++ src/App.vue
@@ -13,37 +13,37 @@ import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
 import TodoFooter from './components/TodoFooter'
 export default {
-  data: function(){
+  data(){
         return {
             todoItems: [],
         }
     },
     methods:{
-      addOneItem: function(todoItem){
-         let obj = {completed: false, item: todoItem};
+      addOneItem(todoItem){
+         const obj = {completed: false, item: todoItem};
          // 저장하는 로직
          localStorage.setItem(todoItem , JSON.stringify(obj));
          this.todoItems.push(obj);
       },
-      removeOneItem: function(todoItem, index){
+      removeOneItem(todoItem, index){
         // Key 값 으로 로컬스트리지 데이터 remove
             localStorage.removeItem(todoItem.item);
             // 실시간 반영을 위해 data 에서도 삭제 
             this.todoItems.splice(index,1);
       },
-      toggleOneItem: function(todoItem,index){
+      toggleOneItem(todoItem,index){
         this.todoItems[index].completed = !this.todoItems[index].completed
         localStorage.removeItem(todoItem.item);
         localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
       },
-      clearAllItem: function(){
+      clearAllItem(){
         localStorage.clear();
         this.todoItems = [];
       }
     },
    // 라이프사이클 : created
     // 인스턴스가 생성되자마자 호출되는 라이프사이클 훅
-    created: function(){
+    created(){
         if(localStorage.length > 0){
             for (let i = 0; i < localStorage.length; i++){
                 if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
@@ -54,10 +54,10 @@ export default {
         }
     },
   components: {
-    'TodoHeader' : TodoHeader,
-    'TodoInput' : TodoInput,
-    'TodoList' : TodoList,
-    'TodoFooter' : TodoFooter,
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
   }
 }
 </script>
